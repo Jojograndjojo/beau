32a9d53b77fdb77132906182521641dda57a15ab
Jonathan <j_passe@live.fr>
2017-02-03 14:49:18 +0000

    change links to button on homepage

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -2,6 +2,10 @@ class HomepagesController < ApplicationController
 
   def index
     @homepage_picture = Artpiece.all.shuffle.first
+    @drawings = Artpiece.where(type_of_art: 'Drawing')
+    @paintings = Artpiece.where(type_of_art: 'Painting')
+    @printmakings = Artpiece.where(type_of_art: 'Printmaking')
+    @artTypes = [@drawings,@paintings,@printmakings]
   end
 
 
